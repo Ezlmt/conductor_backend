5d45e88dffe442d559b17961ee34834d03e40479
ezlmt <2876187491@qq.com>
2025-12-28 00:30:03 -0800

    feat: soft delete course
    1. The data will be soft delete when professor delete a course
    2. add an delete-info api for detect the number of enroll of a course

--- internal/models/course.go
+++ internal/models/course.go
@@ -1,11 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Course struct {
-	ID          uint      `gorm:"primaryKey" json:"id"`
-	Name        string    `gorm:"not null" json:"name"`
-	Code        string    `gorm:"not null" json:"code"`
-	ProfessorID uint      `gorm:"not null" json:"professorId"`
-	CreatedAt   time.Time `gorm:"not null" json:"createdAt"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	Name        string         `gorm:"not null" json:"name"`
+	Code        string         `gorm:"not null" json:"code"`
+	ProfessorID uint           `gorm:"not null" json:"professorId"`
+	CreatedAt   time.Time      `gorm:"not null" json:"createdAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
